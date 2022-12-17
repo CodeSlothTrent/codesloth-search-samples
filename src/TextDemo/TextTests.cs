eefbdd3463edece8539fac204684173396faef6c
TJ <trentjsky@gmail.com>
2022-12-17 20:47:18 +1100

    Demonstrating max token length on standard analyzer

--- src/TextDemo/TextTests.cs
+++ src/TextDemo/TextTests.cs
@@ -83,7 +83,7 @@ namespace TextDemo
             "great:1, product:1",
             "The english stopword defaults are defined in the link above and also include those which we specified manually in the prior test case")
         ]
-        public async Task TextMapping_CanBeConfiguredToUseStopWordsTokenFilterToOmitWordsAsTokens(string text, string[] stopWords, string expectedTokensCsv, string explanation)
+        public async Task TextMapping_StandardAnalyzer_CanBeConfiguredToUseStopWordsTokenFilterToOmitWordsAsTokens(string text, string[] stopWords, string expectedTokensCsv, string explanation)
         {
             var indexName = "text-index";
             var customAnalyzerName = "customAnalyzer";
@@ -96,6 +96,66 @@ namespace TextDemo
                     .Analyzers(analyzers => analyzers
                     // Set stopwords to our custom list
                     .Standard(customAnalyzerName, selector => selector.StopWords(stopWords)
+                    ))
+                ),
+                mapping => mapping
+                    .Properties<ProductDocument>(propertyDescriptor => propertyDescriptor
+                        .Text(word => word
+                            .Name(name => name.Description)
+                            // Set our custom analyzer on this text mapping so that we apply the stop token filter during indexing
+                            .Analyzer(customAnalyzerName)
+                    )),
+                async (uniqueIndexName, opensearchClient) =>
+                {
+                    var productDocument = new ProductDocument(1, text);
+
+                    await _fixture.IndexDocuments(uniqueIndexName, new[] { productDocument });
+
+                    // TermVectors will return us the indexed tokens for our field
+                    var result = await opensearchClient.TermVectorsAsync<ProductDocument>(selector => selector
+                            .Index(uniqueIndexName)
+                            .Document(productDocument)
+                        );
+
+                    result.IsValid.Should().BeTrue();
+                    var tokensAndFrequency = string.Join(", ", result.TermVectors.Values.SelectMany(value => value.Terms.Select(term => $"{term.Key}:{term.Value.TermFrequency}")));
+                    tokensAndFrequency.Should().BeEquivalentTo(expectedTokensCsv, explanation);
+                }
+            );
+        }
+
+        [Theory]
+        /// <summary>
+        /// This test creates a new custom analyzer of type "standard" (the standard analyzer), which leverages its default tokenizers and token filters
+        /// and applies custom maximum token length
+        /// </summary>
+        [
+            InlineData(
+            "GREAT product!",
+            3,
+            "at:1, duc:1, gre:1, pro:1, t:1",
+            "The words are decomposed into smaller tokens: great = gre + at, product = pro + duc + t")
+        ]
+        [
+            InlineData(
+            "GREAT product!",
+            5,
+            "ct:1, great:1, produ:1",
+            "The words are decomposed into smaller tokens: great = great, product = produ + ct")
+        ]
+        public async Task TextMapping_StandardAnalyzer_CanBeConfiguredWithMaximumTokenLength(string text, int maxTokenLength, string expectedTokensCsv, string explanation)
+        {
+            var indexName = "text-index";
+            var customAnalyzerName = "customAnalyzer";
+
+            await _fixture.PerformActionInTestIndex<ProductDocument>(
+                indexName,
+                 settings => settings
+                // We can configure our custom analyzer via settings on the index
+                .Analysis(analysis => analysis
+                    .Analyzers(analyzers => analyzers
+                    // Set stopwords to our custom list
+                    .Standard(customAnalyzerName, selector => selector.MaxTokenLength(maxTokenLength)
                     ))
                 ),
                 mapping => mapping
