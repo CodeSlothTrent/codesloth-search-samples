d03f2542ff8928d2dd7b2abe4c50cf8f8aa0532b
TJ <trentjsky@gmail.com>
2023-01-09 11:42:11 +1100

    Adding test to demonstrate ascii sorting on numbers

--- src/KeywordDemo/KeywordSortingTests.cs
+++ src/KeywordDemo/KeywordSortingTests.cs
@@ -62,7 +62,7 @@ namespace KeywordDemo
 
         [Fact]
         /// <summary>
-        /// Keyword fields do not require anything special to support sorting, such as the FieldData mapping that text fields might
+        /// Keyword fields do not require anything special to support sorting
         /// </summary>
         public async Task KeywordMapping_CanBeUsedAsASortedField_WithoutAnySpecialConsiderations()
         {
@@ -92,6 +92,39 @@ namespace KeywordDemo
                     result.IsValid.Should().BeTrue();
                     var formattedResults = string.Join(", ", result.Documents.Select(doc => doc.Name));
                     formattedResults.Should().BeEquivalentTo("mouse pad, mouse");
+                }
+            );
+        }
+
+        [Fact]
+        public async Task KeywordMapping_ShouldNotBeUsedToSortNumericData()
+        {
+            var indexName = "keyword-index";
+            await _fixture.PerformActionInTestIndex(
+                indexName,
+                mappingDescriptor,
+                async (uniqueIndexName, opensearchClient) =>
+                {
+                    var productDocuments = new[] {
+    new ProductDocument(1, "5"),
+    new ProductDocument(2, "2000"),
+};
+
+                    await _fixture.IndexDocuments(uniqueIndexName, productDocuments);
+
+                    var result = await opensearchClient.SearchAsync<ProductDocument>(selector => selector
+                           .Index(uniqueIndexName)
+                           .Query(query => query.MatchAll())
+                           .Explain()
+                           .Sort(sort => sort
+                            .Descending(fieldName => fieldName.Name)
+                        )
+                    );
+
+                    // Our documents can be sorted alphabetically
+                    result.IsValid.Should().BeTrue();
+                    var formattedResults = string.Join(", ", result.Documents.Select(doc => doc.Name));
+                    formattedResults.Should().BeEquivalentTo("2000, 5");
                 }
             );
         }
